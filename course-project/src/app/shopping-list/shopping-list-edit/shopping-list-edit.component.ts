d4e9b03501b63038959c031a3d364d5919d27703
abdoul06 <82101190+abdoul06@users.noreply.github.com>
2021-06-30 10:05:10 -0300

    directives have added

--- course-project/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ course-project/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,8 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from "@angular/core";
+import { Ingredient } from "src/app/shared/ingredient.model";
 
 @Component({
     selector: 'app-shopping-list-edit',
     templateUrl: './shopping-list-edit.component.html',
     styleUrls: ['./shopping-list-edit.component.css']
 })
-export class shoppingListEditComponent {}
+export class shoppingListEditComponent {
+    // @ViewChild('nameInput', {static: true}) nameInput: ElementRef;
+    // @ViewChild('amountInput', {static: true}) amountInput: ElementRef;
+
+    @Output() ingredientCreated = new EventEmitter<Ingredient>();
+
+    onAddIngredient(nameInput, amountInput) {
+        // const ingName = this.nameInput.nativeElement.value;
+        // const ingAmount = this.amountInput.nativeElement.value;
+        // const newIngredient = new Ingredient(ingName,ingAmount)
+        
+        this.ingredientCreated.emit({
+            name: nameInput.value,
+            amount: amountInput.value
+        });
+    }
+}
