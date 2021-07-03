e7dd2c2ebed839a0a24dad23474068f22cd98e64
abdoul06 <82101190+abdoul06@users.noreply.github.com>
2021-07-03 12:58:21 -0300

    this folder will not be updated anymore

--- course-project/src/app/recipe/recipe.component.ts
+++ course-project/src/app/recipe/recipe.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
-  styleUrls: ['./recipe.component.css']
+  styleUrls: ['./recipe.component.css'],
+  providers: []
 })
 export class RecipeComponent implements OnInit {
 
