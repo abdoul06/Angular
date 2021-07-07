d793de6f45558e33b2c3b1c58913f7f40fb9f011
abdoul06 <82101190+abdoul06@users.noreply.github.com>
2021-07-07 15:17:33 -0300

    operators and subject have been added

--- 13.observable/obs-01-start/src/app/home/home.component.ts
+++ 13.observable/obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -30,11 +31,15 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000)
     });
     
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+    this.firstObsSubscription = customIntervalObservable.pipe(filter(data => {
+      return data > 0;
+    }),map( (data: number) => {
+      return 'Round: ' +  (data+1);
+    })).subscribe(data => {
       console.log(data)
     }, error => {
       console.log(error)
-      alert(error.message)
+      // alert(error.message)
     }, () => {
       console.log('Completed!!!')
     }
