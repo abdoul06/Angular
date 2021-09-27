a5f27518091c43c4cfc3ffcbacd2ced6b74ec11a
Abdillahi Hussein <abdillahi.hussein@ibm.com>
2021-09-27 14:51:06 -0300

    form tamplate driven

--- 15.forms/forms-td-start/src/app/app.component.ts
+++ 15.forms/forms-td-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,20 +7,20 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signUpForm: NgForm; 
-  defaultQuestion = 'teacher';
+  @ViewChild('f') signUpForm: NgForm;  
+  defaultQuestion = 'pet';
   answer = '';
-  genders = ['male', 'female']
+  genders = ['Male', 'Female'];
   user = {
     username: '',
     email: '',
-    secretQuesiton: '',
+    secretQuestion: '',
     answer: '',
     gender: ''
   }
-  submitted = false; 
-
-  suggestUserName() {
+  submitted = false;
+  
+  suggestedUsername() {
     const suggestedName = 'Superuser';
     // this.signUpForm.setValue({
     //   userData: {
@@ -29,33 +29,23 @@ export class AppComponent {
     //   },
     //   secret: 'pet',
     //   questionAnswer: '',
-    //   gender: 'male'
-    // });
+    //   gender: 'Male'
+    // })
     this.signUpForm.form.patchValue({
       userData: {
         username: suggestedName
       }
     });
   }
-
   onSubmit() {
-    this.submitted = true;
+    this.submitted = true; 
     this.user.username = this.signUpForm.value.userData.username;
     this.user.email = this.signUpForm.value.userData.email;
-    this.user.secretQuesiton = this.signUpForm.value.secret;
+    this.user.secretQuestion = this.signUpForm.value.secret;
     this.user.answer = this.signUpForm.value.questionAnswer;
     this.user.gender = this.signUpForm.value.gender;
-    
-    // this.signUpForm.reset();
-    // this.signUpForm.setValue({
-    //   userData: {
-    //     username: '',
-    //     email: ''
-    //   },
-    //   secret: '',
-    //   questionAnswer: '',
-    //   gender: ''
-    // });
+
+    this.signUpForm.reset()
+
   }
-  
 }
